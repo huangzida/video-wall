8821cc2872ebdd443edc5bd445ce5d73f1972cf7
huangzida <398926656@qq.com>
2026-03-21 23:27:17 +0800

    feat: add package entry point

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { VideoWall } from './components/VideoWall';
+export type { VideoWallProps, VideoWallRef } from './types';
+export type { Cell, Layout, WindowConfig, WindowState, PresetLayout } from './types';
